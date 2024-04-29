d8eb68e02ae938cced56ecd7d2ff5c3174c3405c
Miguel Gil <2023102850@uac.pt>
2024-04-29 17:14:17 +0000

    Starting Fleet

---
+++ html/book.php
@@ -0,0 +1,5 @@
+<?php 
+require_once './components/header.php';
+
+echo getHeader();
+?>
---
+++ html/fleet.php
@@ -0,0 +1,13 @@
+<?php 
+require_once './components/header.php';
+require_once './components/nav.php';
+
+echo getHeader();
+?>
+
+<body>
+    <?php echo getNav();?>
+
+</body>
+
+</html>
