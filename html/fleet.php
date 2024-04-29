251ed5bd381479c1713801dd88e40f37cc7ad371
Miguel Gil <2023102850@uac.pt>
2024-04-29 17:15:33 +0000

    changes to fleet

--- html/fleet.php
+++ html/fleet.php
@@ -8,6 +8,14 @@ echo getHeader();
 <body>
     <?php echo getNav();?>
 
+    <div class="container">
+        <div class="text-content" style="position: relative; top:150px;">
+            <h1>Fleet</h1>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+    </script>
 </body>
 
 </html>
