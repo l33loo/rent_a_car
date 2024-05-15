8bb73c0744bb49d0dc88ff2c2dc7a9de9d045a50
Miguel Gil <2023102850@uac.pt>
2024-05-15 21:35:17 +0000

    dropdown fixed

--- src/index.php
+++ src/index.php
@@ -54,108 +54,116 @@ $cityNames = Address::search([]);
         </div>
     </div>
 
-    <form action="" method="post">
-        <div class="container my-5 w-50"
-            style="position: relative; top: -250px; background-color: rgba(189, 195, 199, 0.8); padding: 15px;border-radius: 15px;">
-            <div class="row" style="text-align: center; ">
-                <div class="col-md-4" style="padding-left: 40px;">
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" name="pickupBtn"
-                            data-bs-toggle="dropdown" aria-expanded="false"
-                            style="padding-right: 22px; background-color: rgba(0,0,0,0.7); color:white">
-                            Pick-up Location
-                        </button>
-                        <select name="city">
-                            <?php foreach ($cityNames as $cityName) : ?>
-                            <option>
-                                <?php echo ($cityName); ?></option>
-                            <?php endforeach; ?>
-                        </select>
-                    </div>
+    <div class="container my-5 w-50"
+        style="position: relative; top: -250px; background-color: rgba(189, 195, 199, 0.8); padding: 15px;border-radius: 15px;">
+        <div class="row" style="text-align: center; ">
+            <div class="col-md-4" style="padding-left: 40px;">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        style="padding-right: 22px; background-color: rgba(0,0,0,0.7); color:white">
+                        Pick-up Location
+                    </button>
+                    <select class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <?php foreach ($cityNames as $cityName) : ?>
+                        <option value="<?php echo ($cityName); ?>">
+                            <?php echo ($cityName); ?></option>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
-
-                <div class="col-md-2" style="padding-right: 25px;">
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false"
-                            style="padding-left: 20px; background-color: rgba(0,0,0,0.7); color:white">
-                            Pick-up Date
-                        </button>
-                        <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item"><input type="date" name="pickDate"></a></li>
-                        </ul>
-                    </div>
-                    <div class="dropdown" style="margin-top: 15px;">
-                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false"
-                            style="background-color: rgba(0,0,0,0.7); color:white">
-                            Drop-off Date
-                        </button>
-                        <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item"><input type="date" name="dropDate"></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-4" style="padding-left:60px;">
-                    <div class="dropdown">
-                        <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false"
-                            style="padding-left: 20px;background-color: rgba(0,0,0,0.7); color:white">
-                            Pick-up Time
-                        </button>
-                        <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item">9:30</a></li>
-                            <li><a class="dropdown-item">9:30</a></li>
-                            <li><a class="dropdown-item">10:00</a></li>
-                            <li><a class="dropdown-item">10:30</a></li>
-                            <li><a class="dropdown-item">11:00</a></li>
-                            <li><a class="dropdown-item">11:30</a></li>
-                            <li><a class="dropdown-item">12:00</a></li>
-                            <li><a class="dropdown-item">12:30</a></li>
-                            <li><a class="dropdown-item">13:00</a></li>
-                            <li><a class="dropdown-item">13:30</a></li>
-                            <li><a class="dropdown-item">14:00</a></li>
-                            <li><a class="dropdown-item">14:30</a></li>
-                            <li><a class="dropdown-item">15:00</a></li>
-                            <li><a class="dropdown-item">15:30</a></li>
-                            <li><a class="dropdown-item">16:00</a></li>
-                            <li><a class="dropdown-item">16:30</a></li>
-                            <li><a class="dropdown-item">17:00</a></li>
-                        </ul>
-                    </div>
-                    <div class="dropdown" style="margin-top: 15px;">
-                        <button class=" btn dropdown-toggle" type="button" id="dropdownMenuButton1"
-                            data-bs-toggle="dropdown" aria-expanded="false"
-                            style="background-color: rgba(0,0,0,0.7); color:white">
-                            Drop-Off Time
-                        </button>
-                        <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                            <li><a class="dropdown-item"></a></li>
-                            <li><a class="dropdown-item">9:30</a></li>
-                            <li><a class="dropdown-item">10:00</a></li>
-                            <li><a class="dropdown-item">10:30</a></li>
-                            <li><a class="dropdown-item">11:00</a></li>
-                            <li><a class="dropdown-item">11:30</a></li>
-                            <li><a class="dropdown-item">12:00</a></li>
-                            <li><a class="dropdown-item">12:30</a></li>
-                            <li><a class="dropdown-item">13:00</a></li>
-                            <li><a class="dropdown-item">13:30</a></li>
-                            <li><a class="dropdown-item">14:00</a></li>
-                            <li><a class="dropdown-item">14:30</a></li>
-                            <li><a class="dropdown-item">15:00</a></li>
-                            <li><a class="dropdown-item">15:30</a></li>
-                            <li><a class="dropdown-item">16:00</a></li>
-                            <li><a class="dropdown-item">16:30</a></li>
-                            <li><a class="dropdown-item">17:00</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-md-1 d-flex justify-content-center align-items-center">
-                    <button type="button" class="btn btn-outline-dark btn-lg" style="margin-left: 40px;">Search</button>
+                <div class="dropdown" style="margin-top: 15px;">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown"
+                        aria-expanded="false" style="background-color: rgba(0,0,0,0.7); color:white">
+                        Drop-Off Location
+                    </button>
+                    <select class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <?php foreach ($cityNames as $cityName) : ?>
+                        <option value="<?php echo ($cityName); ?>">
+                            <?php echo ($cityName); ?></option>
+                        <?php endforeach; ?>
+                    </select>
                 </div>
             </div>
+            <div class="col-md-2" style="padding-right: 25px;">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                        style="padding-left: 20px; background-color: rgba(0,0,0,0.7); color:white">
+                        Pick-up Date
+                    </button>
+                    <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <input class="dropdown-item" type="date" name="" id="">
+                    </ul>
+                </div>
+                <div class="dropdown" style="margin-top: 15px;">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false"
+                        style="background-color: rgba(0,0,0,0.7); color:white">
+                        Drop-off Date
+                    </button>
+                    <ul class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <input class="dropdown-item" type="date" name="" id="">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4" style="padding-left:60px;">
+                <div class="dropdown">
+                    <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown"
+                        aria-expanded="false" style="padding-left: 20px;background-color: rgba(0,0,0,0.7); color:white">
+                        Pick-up Time
+                    </button>
+                    <select class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <option class="dropdown-item" href="#">9:30</option>
+                        <option class="dropdown-item" href="#">10:00</option>
+                        <option class="dropdown-item" href="#">10:30</option>
+                        <option class="dropdown-item" href="#">11:00</option>
+                        <option class="dropdown-item" href="#">11:30</option>
+                        <option class="dropdown-item" href="#">12:00</option>
+                        <option class="dropdown-item" href="#">12:30</option>
+                        <option class="dropdown-item" href="#">13:00</option>
+                        <option class="dropdown-item" href="#">13:30</option>
+                        <option class="dropdown-item" href="#">14:00</option>
+                        <option class="dropdown-item" href="#">14:30</option>
+                        <option class="dropdown-item" href="#">15:00</option>
+                        <option class="dropdown-item" href="#">15:30</option>
+                        <option class="dropdown-item" href="#">16:00</option>
+                        <option class="dropdown-item" href="#">16:30</option>
+                        <option class="dropdown-item" href="#">17:00</option>
+                        <option class="dropdown-item" href="#">17:30</option>
+                    </select>
+                </div>
+                <div class="dropdown" style="margin-top: 15px;">
+                    <button class=" btn dropdown-toggle" type="button" id="dropdownMenuButton1"
+                        data-bs-toggle="dropdown" aria-expanded="false"
+                        style="background-color: rgba(0,0,0,0.7); color:white">
+                        Drop-Off Time
+                    </button>
+                    <select class=" dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <option class="dropdown-item" href="#">9:30</option>
+                        <option class="dropdown-item" href="#">10:00</option>
+                        <option class="dropdown-item" href="#">10:30</option>
+                        <option class="dropdown-item" href="#">11:00</option>
+                        <option class="dropdown-item" href="#">11:30</option>
+                        <option class="dropdown-item" href="#">12:00</option>
+                        <option class="dropdown-item" href="#">12:30</option>
+                        <option class="dropdown-item" href="#">13:00</option>
+                        <option class="dropdown-item" href="#">13:30</option>
+                        <option class="dropdown-item" href="#">14:00</option>
+                        <option class="dropdown-item" href="#">14:30</option>
+                        <option class="dropdown-item" href="#">15:00</option>
+                        <option class="dropdown-item" href="#">15:30</option>
+                        <option class="dropdown-item" href="#">16:00</option>
+                        <option class="dropdown-item" href="#">16:30</option>
+                        <option class="dropdown-item" href="#">17:00</option>
+                        <option class="dropdown-item" href="#">17:30</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-1 d-flex justify-content-center align-items-center">
+                <button type="button" class="btn btn-outline-dark btn-lg" style="margin-left: 40px;">Search</button>
+            </div>
         </div>
-    </form>
+    </div>
 
     <div class="container">
         <h2 style="position:relative; top: -100px;">Our Fleet</h2>
