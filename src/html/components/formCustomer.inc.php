404040fb66b43d02647902cd155edf215b47fe88
Lila Karpowicz <lila@karpowicz.dev>
2024-05-23 11:27:59 +0000

    front-end admin reservationEdit

---
+++ src/html/components/formCustomer.inc.php
@@ -0,0 +1,54 @@
+<fieldset>
+    <Legend>
+        <img src="/img/profile.svg" alt="" style="height: 20px; width:20px; margin-bottom:10px;">
+        Customer
+    </Legend>
+    <div class="row mb-3">
+        <div class="col">
+            <img src="/img/profile.svg" alt="" style="height: 20px; width:20px; margin-bottom:10px;">
+            <label for="name">
+                Name:
+            </label>
+            <input type="text" class="form-control" name="name">
+        </div>
+        <div class="col">
+            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
+            <label for="email">
+                Email:
+            </label>
+            <input type="email" class="form-control" name="email">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col">
+            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
+            <label for="phone">
+                Phone:
+            </label>
+            <input type="text" class="form-control" name="phone">
+        </div>
+        <div class="col">
+            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
+            <label for="dateOfBirth">
+                Birthdate:
+            </label>
+            <input type="date" class="form-control" name="dateOfBirth">
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col">
+            <img src="/img/password.svg" alt="" style="height: 20px; width:20px; margin-bottom:10px;">
+            <label for="driversLicense">
+                Driver's License:
+            </label>
+            <input type="text" class="form-control" name="driversLicense">
+        </div>
+        <div class="col">
+            <img src="/img/password.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
+            <label for="taxNumber">
+                Tax Number (optional):
+            </label>
+            <input type="text" class="form-control" name="taxNumber">
+        </div>
+    </div>
+</fieldset>
--- src/html/components/reservationBookForm.inc.php
+++ src/html/components/reservationBookForm.inc.php
@@ -1,73 +1,7 @@
 <form action="/app/reservationBook.php" method="post">
-    <div class="row mb-3">
-        <div class="col">
-            <img src="/img/profile.svg" alt="" style="height: 20px; width:20px; margin-bottom:10px;">
-            <label for="name">
-                Name:
-            </label>
-            <input type="text" class="form-control" name="name">
-        </div>
-        <div class="col">
-            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
-            <label for="email">
-                Email:
-            </label>
-            <input type="email" class="form-control" name="email">
-        </div>
-    </div>
-    <div class="row mb-4">
-        <div class="col">
-            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
-            <label for="phone">
-                Phone:
-            </label>
-            <input type="text" class="form-control" name="phone">
-        </div>
-        <div class="col">
-            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
-            <label for="dateOfBirth">
-                Birthdate:
-            </label>
-            <input type="date" class="form-control" name="dateOfBirth">
-        </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col">
-            <img src="/img/password.svg" alt="" style="height: 20px; width:20px; margin-bottom:10px;">
-            <label for="driversLicense">
-                Driver's License:
-            </label>
-            <input type="text" class="form-control" name="driversLicense">
-        </div>
-        <div class="col">
-            <img src="/img/password.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
-            <label for="taxNumber">
-                Tax Number (optional):
-            </label>
-            <input type="text" class="form-control" name="taxNumber">
-        </div>
-    </div>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/html/components/formCustomer.inc.php'; ?>
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/html/components/formAddress.inc.php'; ?>
-    <fieldset class="mb-3">
-        <legend>
-            <img src="/img/email.svg" alt="" style="height: 20px; width:20px; margin-bottom:5px;">
-            Payment
-        </legend>
-        <div class="row mb-4">
-            <div class="col-12 col-md-8">
-                <label for="ccNumber">Credit Card Number</label>
-                <input type="text" class="form-control" name="ccNumber">
-            </div>
-            <div class="col-12 col-md">
-                <label for="ccExpiry">Expiry</label>
-                <input type="date" class="form-control" name="ccExpiry">
-            </div>
-            <div class="col-12 col-md">
-                <label for="ccCVV">CVV</label>
-                <input type="text" class="form-control" name="ccCVV">
-            </div>
-        </div>
-    </fieldset>
+    <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/html/components/formPayment.inc.php'; ?>
     <div class="d-flex justify-content-center">
         <input type="hidden" name="userId" value="<?php echo empty($_SESSION['logged_id']) ? null : $_SESSION['logged_id'] ?>">
         <input type="hidden" name="categoryId" value="<?php echo $_GET['categoryId'] ?>">
