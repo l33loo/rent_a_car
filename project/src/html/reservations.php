b45290f17ec0ea39a43fc53341d0c58893423690
Lila Karpowicz <lila@karpowicz.dev>
2024-05-28 22:02:58 +0000

    finish user vehicles card

--- project/src/html/reservations.php
+++ project/src/html/reservations.php
@@ -17,85 +17,89 @@ echo getHeader();
 
 <body>
     <?php include 'components/navbar.inc.php'; ?>
-    <div class="container mt-5">
-        <h1>Your Reservations</h1>
+    <div class="container mt-5 flex-grow-1">
+        <h1 class="mb-4">Your Reservations</h1>
         <?php foreach ($revisions as $revision) {
             $vehicle = $revision->loadVehicle()->getVehicle();
             $vehicle->loadProperties();
             $category = $revision->loadCategory()->getCategory();
             $categoryProperties = $category->loadProperties()->getProperties();
-            print_r($vehicle);
         ?>
             <div class="card mb-3">
                 <div class="row g-0">
                     <div class="col-md-4" style="overflow: hidden;">
                         <img src="/src/img/car.jpg" class="img-fluid rounded-start" alt="" style="object-fit: cover; height: 300px; width: 100%">
                     </div>
-                    <div class="col-md-5">
+                    <div class="col-md-6">
                         <div class="card-body">
                             <h2 class="card-title">
-                                <?php echo $vehicle->Brand . ' ' . $vehicle->Model ?>
+                                <?php echo $vehicle->Brand . ' ' . $vehicle->Model ?> <small>(<?php echo $category->getName() ?>)</small>
                             </h2>
-                            <div class="row">
+                            <div>
                                 <?php foreach ($categoryProperties as $categoryProperty) { ?>
-                                    <div class="col-6">
-                                        <?php echo $categoryProperty->getName() . ': ' . $categoryProperty->getPropertyValue() ?>
-                                    </div>
+                                    <span class="p-1 m-1 bg-warning-subtle"><?php echo $categoryProperty->getName() . ': ' . $categoryProperty->getPropertyValue() ?></span>
                                 <?php } ?>
+                            </div>
+                            <div class="table-responsive mt-4">
+                                <table class="table table-borderless">
+                                    <thead>
+                                        <tr>
+                                            <th></th>
+                                            <th>
+                                                Pick-up
+                                            </th>
+                                            <th>
+                                                Drop-off
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <th>Location</th>
+                                            <td>
+                                                <?php echo $revision->loadPickupLocation()->getPickupLocation()->getName() ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $revision->loadDropoffLocation()->getDropoffLocation()->getName() ?>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Date</th>
+                                            <td>
+                                                <?php echo $revision->getPickupDate() ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $revision->getDropoffDate() ?>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <th>Time</th>
+                                            <td>
+                                                <?php echo $revision->getPickupTime() ?>
+                                            </td>
+                                            <td>
+                                                <?php echo $revision->getDropoffTime() ?>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-3 d-flex">
-                        <div class="card-body d-flex flex-column justify-content-evenly">
-                            <div>
-                                <div class="h3 text-end">
+                    <div class="col-md-2 d-flex">
+                        <div class="card-body d-flex flex-column justify-content-evenly bg-secondary-subtle">
+                            <div class="text-center">
+                                <div class="h3 ">
                                     <?php echo $category->getDailyRateToString() ?> <small>per day</small>
                                 </div>
-                                <div class="h5 text-end">
-                                    <?php echo 'Total: ' . $revision->getTotalPrice(); ?>
+                                <div class="h5">
+                                    <?php echo 'Total: ' . $revision->getTotalPriceToString(); ?>
                                 </div>
                             </div>
-                            <div class="d-flex flex-wrap justify-content-end align-items-end" style="gap:10px;">
+                            <div class="d-flex flex-wrap justify-content-center" style="gap:10px;">
                                 <a class="btn btn-primary ml-2" href="/src/html/reservationView.php">View</a>
                                 <a class="btn btn-secondary ml-2" href="/src/html/reservationEdit.php">Edit</a>
                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col">
-                    <img src="/src/img/car.jpg" alt="">
-                </div>
-                <div class="col">
-                    <h2>
-                        
-                    </h2>
-                    <div>
-                        Or similar <?php echo $category->getName() ?> car
-                    </div>
-                    <div class="row">
-                        <?php foreach ($categoryProperties as $categoryProperty) { ?>
-                            <div class="col-6">
-                                <?php echo $categoryProperty->getName() . ': ' . $categoryProperty->getPropertyValue() ?>
-                            </div>
-                        <?php } ?>
-                    </div>
-                </div>
-                <div class="col-auto">
-                    <div class="h3">
-                        <?php echo $category->getDailyRateToString() ?> <small>per day</small>
-                    </div>
-                    <div class="h5">
-                        <?php echo 'Total: ' . $revision->getTotalPrice(); ?>
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <a class="btn btn-primary" href="/src/html/reservationView.php">View</a>
-                        </div>
-                        <div class="col">
-                            <a class="btn btn-secondary" href="/src/html/reservationEdit.php">Edit</a>
                         </div>
                     </div>
                 </div>
