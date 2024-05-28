0f34adbd735b770d94206f97d5910a533af96168
Lila Karpowicz <lila@karpowicz.dev>
2024-05-28 20:10:32 +0000

    delete custom autoload file

--- project/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register("autoLoader");
-function autoLoader($classACarregar)
-{
-    $ficheiroAcarregar = str_replace('\\', DIRECTORY_SEPARATOR, $classACarregar)
-        . ".php";
-    require_once $ficheiroAcarregar;
-}
