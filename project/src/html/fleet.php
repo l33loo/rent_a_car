921b5bb4f226d11e7f279033a0a5f5451f5e0e42
Lila Karpowicz <lila@karpowicz.dev>
2024-06-01 15:00:20 +0000

    Fix fleet page

--- project/src/html/fleet.php
+++ project/src/html/fleet.php
@@ -1,186 +1,34 @@
 <?php 
-require_once './components/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/src/html/components/header.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+
+use RentACar\Category;
+
+$categories = Category::search([]);
 
 echo getHeader();
 ?>
 
 <body>
-    <?php include 'components/navbar.inc.php'; ?>
-    <div class="container">
-        <div class="text-content">
-            <h1 style="padding-top: 150px;">Fleet</h1>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/html/components/navbar.inc.php'; ?>
+    <div class="fleet container">
+        <h1 class="title">Our Fleet</h1>
+        <div class="d-flex flex-wrap justify-content-around">
+            <?php foreach ($categories as $category): ?>
+            <div class="card">
+                <img class="card-img-top" src="/src/img/<?php echo $category->getName(); ?>.jpg" alt="">
+                <div class="card-info">
+                    <h2><?php echo $category->getName(); ?></h2>
+                    <p><?php echo $category->getDescription(); ?></p>
+                    <p>Daily Rate: <?php echo number_format($category->getDailyRate(), 2); ?> €</p>
+                    <a href="/src/html/categoryView.php?categoryId=<?php echo $category->getId(); ?>"
+                        class="btn btn-primary">View Details</a>
+                </div>
+            </div>
+            <?php endforeach; ?>
         </div>
     </div>
-    <div class="container" style="position: relative; top:100px;">
-        <div class="row justify-content-center">
-            <div class="col-md-3">
-                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel" style="height:400px;">
-                    <div class="carousel-inner" style="filter: drop-shadow(20px 20px 20px);">
-                        <div class="carousel-item active" data-bs-interval="10000">
-                            <div class="card h-100">
-                                <img src=" ../img/car.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Regular</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">25.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <div class="card h-100">
-                                <img src="../img/car2.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Regular</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">25.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card h-100">
-                                <img src="../img/car3.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Regular</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">25.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div id="carouselExampleInterval2" class="carousel slide" data-bs-ride="carousel" style="height:400px;">
-                    <div class="carousel-inner" style="filter: drop-shadow(20px 20px 20px);">
-                        <div class="carousel-item active" data-bs-interval="10000">
-                            <div class="card h-100">
-                                <img src="../img/moto.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Motorcycle</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">20.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <div class="card h-100">
-                                <img src="../img/moto2.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Motorcycle</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">20.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card h-100">
-                                <img src="../img/moto3.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Motorcycle</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">20.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval2"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval2"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-            <div class="col-md-3">
-                <div id="carouselExampleInterval3" class="carousel slide" data-bs-ride="carousel" style="height:400px;">
-                    <div class="carousel-inner" style="filter: drop-shadow(20px 20px 20px);">
-                        <div class="carousel-item active" data-bs-interval="10000">
-                            <div class="card h-100">
-                                <img src="../img/suv.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Suv</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">30.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item" data-bs-interval="2000">
-                            <div class="card h-100">
-                                <img src="../img/suv2.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Suv</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">30.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="carousel-item">
-                            <div class="card h-100">
-                                <img src="../img/suv3.jpg" class="card-img-top" alt="suv">
-                                <div class="card-body text-center"
-                                    style="background-color: rgba(25, 135, 84, 0.7); color: white;">
-                                    <h5 class="card-title">Suv</h5>
-                                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis
-                                        ut sapien non urna tincidunt consectetur. Nulla facilisi.</p>
-                                    <span style="font-size: 15px;">Price/day:</span><span
-                                        style="font-size: 20px; margin-left:5px; padding-top:15px; font-weight:bold;">30.00€</span>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval3"
-                        data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval3"
-                        data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php include 'components/footer.inc.php'; ?>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/src/html/components/footer.inc.php'; ?>
 </body>
 
 </html>
