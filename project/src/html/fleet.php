1547fe3d785b1858f730b92b28b2450110bb74f9
Lila Karpowicz <lila@karpowicz.dev>
2024-06-01 16:03:32 +0000

    reservation view and other fixes

--- project/src/html/fleet.php
+++ project/src/html/fleet.php
@@ -17,12 +17,14 @@ echo getHeader();
             <?php foreach ($categories as $category): ?>
             <div class="card">
                 <img class="card-img-top" src="/src/img/<?php echo $category->getName(); ?>.jpg" alt="">
-                <div class="card-info">
-                    <h2><?php echo $category->getName(); ?></h2>
-                    <p><?php echo $category->getDescription(); ?></p>
-                    <p>Daily Rate: <?php echo $category->getDailyRateToString() ?></p>
+                <div class="card-info d-flex flex-column justify-content-between flex-grow-1">
+                    <div>
+                        <h2><?php echo $category->getName(); ?></h2>
+                        <p><?php echo $category->getDescription(); ?></p>
+                        <p>Daily Rate: <?php echo $category->getDailyRateToString() ?></p>
+                    </div>
                     <a href="/src/html/categoryView.php?categoryId=<?php echo $category->getId(); ?>"
-                        class="btn btn-primary">View Details</a>
+                        class="btn btn-primary align-self-center">View Details</a>
                 </div>
             </div>
             <?php endforeach; ?>
