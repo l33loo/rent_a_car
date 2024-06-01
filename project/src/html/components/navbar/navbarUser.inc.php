f8a173780d4dce674b8207224fb85fd4aba47d6c
Lila Karpowicz <lila@karpowicz.dev>
2024-06-01 11:31:40 +0000

    fix broken links

--- project/src/html/components/navbar/navbarUser.inc.php
+++ project/src/html/components/navbar/navbarUser.inc.php
@@ -3,7 +3,7 @@
     <div><strong>Welcome back, <?php echo $_SESSION['name']; ?>!</strong></div>
     <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mt-4">
         <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/index.php">
+            <a class="nav-link active" aria-current="page" href="">
                 Home
             </a>
         </li>
