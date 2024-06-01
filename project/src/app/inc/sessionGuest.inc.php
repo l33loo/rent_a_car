3999c191a5c09ef4b5194bb6ca92145e3c77aab3
Lila Karpowicz <lila@karpowicz.dev>
2024-06-01 16:25:11 +0000

    prevent non-autorized access to user reservation

--- project/src/app/inc/sessionGuest.inc.php
+++ project/src/app/inc/sessionGuest.inc.php
@@ -5,3 +5,11 @@ if (empty($_SESSION['logged_id'])) {
     header('Location: /src/html/login.php');
     exit;
 }
+
+function isSameUser(int $userId) {
+    return $_SESSION['logged_id'] != $userId;
+}
+
+function redirectLoggedInUser(string $path) {
+    header("Location: $path");
+}
