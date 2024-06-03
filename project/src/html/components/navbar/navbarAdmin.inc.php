376a16307f0c2e308c53fc7b90a4d86b58824b3d
Lila Karpowicz <lila@karpowicz.dev>
2024-06-03 02:13:26 +0000

    fix nav bar

--- project/src/html/components/navbar/navbarAdmin.inc.php
+++ project/src/html/components/navbar/navbarAdmin.inc.php
@@ -1,23 +1,33 @@
 <div class="offcanvas-body">
-    <div>Welcome back, <?php echo $_SESSION['name']; ?>!</div>
-    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+    <div><strong>Welcome back, <?php echo $_SESSION['name']; ?>!</strong></div>
+    <ul class="navbar-nav justify-content-end flex-grow-1 pe-3 mt-4">
         <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="/src/html/admin/dashboard.php">
                 Dashboard
             </a>
         </li>
-        <!-- <li class="nav-item">
-            <a class="nav-link" href="./fleet.php">
-                ADMIN Fleet
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/src/html/admin/vehicles.php">
+                Manage Vehicles
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="./about.php">
-                ADMIN About
+            <a class="nav-link active" aria-current="page" href="/src/html/admin/locations.php">
+                Manage Locations
             </a>
-        </li> -->
+        </li>
         <li class="nav-item">
-            <a class="btn btn-dark" href="/src/app/logout.php" class="nav-link">
+            <a class="nav-link active" aria-current="page" href="/src/html/admin/users.php">
+                Manage Users
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/src/html/admin/reservations.php">
+                Manage Reservations
+            </a>
+        </li>
+        <li class="nav-item mt-3">
+            <a class="btn btn-dark" href="/src/app/logout.php">
                 Logout
             </a>
         </li>
